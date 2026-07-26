5e09eed0a687592d7862da88bbc968ecb1f73dac
Sherdrom <525826386@qq.com>
2026-07-26 12:24:48 +0800

    feat: 切换键仅在玩家按住鼠标右键瞄准（InputType.Aim）时生效

--- .AssemblyCSharpSource/SwitchableRangedWeapon/ClientProject/ClientSource/SwitchableRangedWeapon.cs
+++ .AssemblyCSharpSource/SwitchableRangedWeapon/ClientProject/ClientSource/SwitchableRangedWeapon.cs
@@ -79,13 +79,13 @@ namespace SRW
                 GameMain.Client?.CreateEntityEvent(Item, new Item.ChangePropertyEventData(this.SerializableProperties["triggerReleased".ToIdentifier()], this), true);
             }
 
-            if (fireModeswitchKey.IsHit())
+            if (character.IsKeyDown(InputType.Aim) && fireModeswitchKey.IsHit())
             {
                 currentFireModeSelected += 1;
                 GameMain.Client?.CreateEntityEvent(Item, new Item.ChangePropertyEventData(this.SerializableProperties["currentFireModeSelected".ToIdentifier()], this));
             }
 
-            if (ModeSwitchKey.IsHit())
+            if (character.IsKeyDown(InputType.Aim) && ModeSwitchKey.IsHit())
             {
                 currentProjectileSelected += 1;
                 GameMain.Client?.CreateEntityEvent(Item, new Item.ChangePropertyEventData(this.SerializableProperties["currentProjectileSelected".ToIdentifier()], this));
