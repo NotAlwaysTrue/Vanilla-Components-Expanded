524cbeea5be56778277740ce7e00b7564bb3704d
NotAlwaysTrue <77662224+NotAlwaysTrue@users.noreply.github.com>
2026-08-11 20:15:52 +0800

    V1.4.1
    Fixed errors related to newly added clamppen parameters
    Fixed a potential nil value error

--- Lua/Scripts/CL_PlayRicochetSound.lua
+++ Lua/Scripts/CL_PlayRicochetSound.lua
@@ -6,7 +6,7 @@ VCE.LoadedSounds = {
 Networking.Receive("PlayRicochetSound", function(message)
         local id = message.ReadString().Value
         if VCE.ArmorConfigs[id] == nil then return end
-        if VCE.ArmorConfigs[id].PlaySound == false then return end
+        if VCE.ArmorConfigs[id].PlaySound ~= true then return end
         local RicochetSound = VCE.LoadedSounds[id] or VCE.LoadedSounds.DefaultSound
         local Range = VCE.ArmorConfigs[id].SoundRange or 1000
         local x = message.ReadDouble()
