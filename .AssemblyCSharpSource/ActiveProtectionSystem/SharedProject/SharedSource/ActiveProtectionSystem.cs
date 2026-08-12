0d26413976bfeb1497d3e9bdb4ec777129256677
NotAlwaysTrue <77662224+NotAlwaysTrue@users.noreply.github.com>
2026-08-12 22:09:01 +0800

    Fixed sequence issue in APS Added an property to set amount of condition reduced when interception is successful

--- .AssemblyCSharpSource/ActiveProtectionSystem/SharedProject/SharedSource/ActiveProtectionSystem.cs
+++ .AssemblyCSharpSource/ActiveProtectionSystem/SharedProject/SharedSource/ActiveProtectionSystem.cs
@@ -7,98 +7,42 @@ namespace ActiveProtectionSystem
         public static Dictionary<Item, bool> Projectiles = [];
 
         private Explosion explosion;
-        private float maxspeed;
-        private float minspeed;
-        private Vector2 minsize;
-        private Vector2 maxsize;
-        private float minmass;
-        private float maxmass;
-        private float range;
-        private float probility;
-        private bool useonintercept;
-        private bool removeonintercept;
-        private bool functionininventory;
-        private bool interceptonlyapproaching;
-        private bool createexplosiononintercept;
         private bool isOn;
         private Dictionary<Item,bool> LastCheckedTargets = [];
 
         [Serialize(100f, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public float Range
-        {
-            get { return range; }
-            set { range = value; }
-
-        }
+        public float Range { get; set; }
 
         [Serialize(1f, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public float Probility
-        {
-            get { return probility; }
-            set { probility = value; }
-
-        }
+        public float Probility { get; set; }
 
         [Serialize(50f, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public float MaxSpeed
-        {
-            get { return maxspeed; }
-            set { maxspeed = value; }
-        }
+        public float MaxSpeed { get; set; }
 
         [Serialize(10f, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public float MinSpeed
-        {
-            get { return minspeed; }
-            set { minspeed = value; }
-        }
+        public float MinSpeed { get; set; }
 
         [Serialize(0f, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public float MinMass
-        {
-            get { return minmass; }
-            set { minmass = value; }
-        }
+        public float MinMass { get; set; }
 
         [Serialize(10000f, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public float MaxMass
-        {
-            get { return maxmass; }
-            set { maxmass = value; }
-        }
+        public float MaxMass { get; set; }
 
-        public Vector2 MinSize
-        {
-            get { return minsize; }
-            set { minsize = value; }
-        }
+        [Serialize(0f, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
+        public float ReduceConditionOnIntercept { get; set; }
 
-        public Vector2 MaxSize
-        {
-            get { return maxsize; }
-            set { maxsize = value; }
-        }
+        public Vector2 MinSize { get; set; }
+
+        public Vector2 MaxSize { get; set; }
 
         [Serialize(false, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public bool UseOnIntercept
-        {
-            get { return useonintercept; }
-            set { useonintercept = value; }
-        }
+        public bool UseOnIntercept { get; set; }
 
         [Serialize(false, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public bool RemoveOnIntercept
-        {
-            get { return removeonintercept; }
-            set { removeonintercept = value; }
-        }
+        public bool RemoveOnIntercept { get; set; }
 
         [Serialize(false, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public bool FunctionInInventory
-        {
-            get { return removeonintercept; }
-            set { removeonintercept = value; }
-        }
+        public bool FunctionInInventory { get; set; }
 
         [Editable, Serialize(false, IsPropertySaveable.Yes, description: "Is the device currently on.", alwaysUseInstanceValues: true)]
         public bool IsOn
@@ -112,36 +56,29 @@ namespace ActiveProtectionSystem
         }
 
         [Editable, Serialize(true, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public bool InterceptOnlyApproaching
-        {
-            get { return interceptonlyapproaching; }
-            set { interceptonlyapproaching = value; }
-        }
+        public bool InterceptOnlyApproaching { get; set; }
 
         [Editable, Serialize(false, IsPropertySaveable.Yes, alwaysUseInstanceValues: true)]
-        public bool CreateExplosionsOnIntercept
-        {
-            get { return createexplosiononintercept; }
-            set { createexplosiononintercept = value; }
-        }
+        public bool CreateExplosionsOnIntercept { get; set; }
 
 
         public ActiveProtectionSystem(Item item, ContentXElement element)
             : base(item, element)
         {
-            maxspeed = element.GetAttributeFloat("maxspeed", 0);
-            minspeed = element.GetAttributeFloat("minspeed", 100);
-            minsize = element.GetAttributeVector2("minsize", new Vector2(0, 0));
-            maxsize = element.GetAttributeVector2("maxsize", new Vector2(1000, 1000));
-            minmass = element.GetAttributeFloat("minmass", 0);
-            maxmass = element.GetAttributeFloat("maxmass", 100);
-            range = element.GetAttributeFloat("range", 100);
-            probility = element.GetAttributeFloat("probility", 1);
-            useonintercept = element.GetAttributeBool("useonintercept", false);
-            removeonintercept = element.GetAttributeBool("removeonintercept", false);
-            functionininventory = element.GetAttributeBool("functionininventory", false);
-            interceptonlyapproaching = element.GetAttributeBool("interceptonlyapproaching", true);
-            createexplosiononintercept = element.GetAttributeBool("createexplosiononintercept", false);
+            MaxSpeed = element.GetAttributeFloat("maxspeed", 0);
+            MinSpeed = element.GetAttributeFloat("minspeed", 100);
+            MinSize = element.GetAttributeVector2("minsize", new Vector2(0, 0));
+            MaxSize = element.GetAttributeVector2("maxsize", new Vector2(1000, 1000));
+            MinMass = element.GetAttributeFloat("minmass", 0);
+            MaxMass = element.GetAttributeFloat("maxmass", 100);
+            Range = element.GetAttributeFloat("range", 100);
+            ReduceConditionOnIntercept = element.GetAttributeFloat("reducedcondition", 0);
+            Probility = element.GetAttributeFloat("probility", 1);
+            UseOnIntercept = element.GetAttributeBool("useonintercept", false);
+            RemoveOnIntercept = element.GetAttributeBool("removeonintercept", false);
+            FunctionInInventory = element.GetAttributeBool("functionininventory", false);
+            InterceptOnlyApproaching = element.GetAttributeBool("interceptonlyapproaching", true);
+            CreateExplosionsOnIntercept = element.GetAttributeBool("createexplosiononintercept", false);
 
             foreach (var subElement in element.Elements())
             {
@@ -160,40 +97,41 @@ namespace ActiveProtectionSystem
             if (LastCheckedTargets.Count > 10) { LastCheckedTargets.Clear(); }
             if (Item.Condition <= 0) { return; }
             if (!IsOn) { return; }
-            if (!functionininventory && Item.IsContained) { return; }
+            if (!FunctionInInventory && Item.IsContained) { return; }
             foreach (Item target in Projectiles.ToDictionary().Keys)
             {
                 if (LastCheckedTargets.TryGetValue(target, out bool __)) { continue; }
                 if (target.Removed || target.IsContained) { continue; }
-                if ((target.WorldPosition - Item.WorldPosition).Length() > range) { continue; }
+                if ((target.WorldPosition - Item.WorldPosition).Length() > Range) { continue; }
                 float targetsize = target.body.GetSize().Length();
-                if (targetsize < minsize.Length() || targetsize > maxsize.Length()) { continue; }
-                if (target.Speed < minspeed || target.Speed > maxspeed) { continue; }
+                if (targetsize < MinSize.Length() || targetsize > MaxSize.Length()) { continue; }
+                if (target.Speed < MinSpeed || target.Speed > MaxSpeed) { continue; }
                 if (target.body.Mass < MinMass || target.body.Mass > MaxMass) { continue; }
-                if (!IsApproaching(target.WorldPosition, target.body.LinearVelocity, Item.WorldPosition, Item.body.LinearVelocity) && interceptonlyapproaching) { continue; }
+                if (!IsApproaching(target.WorldPosition, target.body.LinearVelocity, Item.WorldPosition, Item.body.LinearVelocity) && InterceptOnlyApproaching) { continue; }
                 if (Submarine.CheckVisibility(item.SimPosition, target.SimPosition) != null) { continue; }
 
                 LastCheckedTargets[target] = true;
 
                 Item.Use(deltaTime);
 
-                if (createexplosiononintercept)
+                if (CreateExplosionsOnIntercept)
                 {
                     explosion?.Explode(target.WorldPosition, Item);
                 }
 
-                if (Rand.GetRNG(Rand.RandSync.ServerAndClient).NextSingle() > probility) { continue; }
+                if (Rand.GetRNG(Rand.RandSync.ServerAndClient).NextSingle() > Probility) { continue; }
 
-                if (useonintercept)
+                if (UseOnIntercept)
                 {
                     target.Use(deltaTime);
                 }
 
-                target.Condition = 0;
-                if (removeonintercept)
+                if (RemoveOnIntercept)
                 {
                     EntitySpawner.Spawner.AddItemToRemoveQueue(target);
                 }
+
+                target.Condition -= ReduceConditionOnIntercept;
             }
         }
 
